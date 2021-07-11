ca54679eee245f2e22370abb803b8c9dc20763d4
mysan91 <mysans1@live.se>
2021-07-11 18:57:12 +0000

    modify

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('products/', include('products.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index, name='home')
+]
