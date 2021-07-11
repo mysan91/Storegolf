da7774bd9525670e80e01d07549803fa64e495a9
mysan91 <mysans1@live.se>
2021-07-11 18:57:35 +0000

    changes

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
+    products = products.objects.all()
 
     context = {
         'products': products,
